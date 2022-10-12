6ddbc66841098747ecaea89294d62b7f7626780d
DanyilYedelkin <yedelkin.daniel@gmail.com>
2022-10-12 19:19:17 +0200

    First commit, add all files for game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] KeyCode keyOne;
+    [SerializeField] KeyCode keyTwo;
+    [SerializeField] Vector3 moveDirection;
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(keyOne))
+        {
+            GetComponent<Rigidbody>().velocity += moveDirection;
+        } 
+
+        if (Input.GetKey(keyTwo))
+        {
+            GetComponent<Rigidbody>().velocity -= moveDirection;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
